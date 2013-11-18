ef07e7cc4c0dfca801b4b9394607fa0dfd131fe3
thomaswelton <thomaswelton@me.com>
2013-11-18 19:27:23 +0000

    Add eloquent relations for the Film model

--- app/models/Film.php
+++ app/models/Film.php
@@ -4,4 +4,16 @@ class Film extends Eloquent {
 	protected $guarded = array();
 
 	public static $rules = array();
+
+	public function genre(){
+		return $this->belongsTo('Genre');
+	}
+
+	public function actors(){
+		return $this->belongsToMany('Actor', 'film_actors');
+	}
+
+	public function director(){
+		return $this->belongsTo('Director');
+	}
 }
