1c04b94ab0291d8e44dcb399d31344ca2324da14
thomaswelton <thomaswelton@me.com>
2013-11-18 19:28:46 +0000

    Delete scaffold methods. Add real index method to list films and their info

--- app/controllers/FilmsController.php
+++ app/controllers/FilmsController.php
@@ -9,71 +9,9 @@ class FilmsController extends BaseController {
 	 */
 	public function index()
 	{
-        return View::make('films.index');
-	}
-
-	/**
-	 * Show the form for creating a new resource.
-	 *
-	 * @return Response
-	 */
-	public function create()
-	{
-        return View::make('films.create');
-	}
-
-	/**
-	 * Store a newly created resource in storage.
-	 *
-	 * @return Response
-	 */
-	public function store()
-	{
-		//
-	}
-
-	/**
-	 * Display the specified resource.
-	 *
-	 * @param  int  $id
-	 * @return Response
-	 */
-	public function show($id)
-	{
-        return View::make('films.show');
-	}
-
-	/**
-	 * Show the form for editing the specified resource.
-	 *
-	 * @param  int  $id
-	 * @return Response
-	 */
-	public function edit($id)
-	{
-        return View::make('films.edit');
-	}
-
-	/**
-	 * Update the specified resource in storage.
-	 *
-	 * @param  int  $id
-	 * @return Response
-	 */
-	public function update($id)
-	{
-		//
-	}
-
-	/**
-	 * Remove the specified resource from storage.
-	 *
-	 * @param  int  $id
-	 * @return Response
-	 */
-	public function destroy($id)
-	{
-		//
+        return View::make('films.index', array(
+        	'films' => Film::all()
+        ));
 	}
 
 }
--- app/views/films/index.blade.php
+++ app/views/films/index.blade.php
@@ -1,1 +1,17 @@
-index.blade
+@foreach ($films as $film)
+	<h1>{{ $film->title }}</h1>
+	<p>Running time in seconds {{ $film->duration }}</p>
+	<p>Genre: {{ $film->genre->name }}</p>
+	<p>Year: {{ $film->year }}</p>
+	<p>Director: {{ $film->director->name }}</p>
+	<p>{{ $film->description }}</p>
+
+
+	Actors
+	<ul>
+		@foreach ($film->actors as $actor)
+			<li>{{ $actor->name }}</li>
+		@endforeach
+	</ul>
+
+@endforeach
